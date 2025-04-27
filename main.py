7d6e5c06aa797e45a786518616c5f3b01f6bd35f
idkk4501 <justinirvin951@gmail.com>
2025-04-26 20:51:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ import random
 print('starting RPS...')
 
 user = input('rock paper or scissors?:')  
-pcch = ["rock", "paper", "scissors]
+pcch = ["rock", "paper", "scissors"]
 program = random.choice(pcch)
 
-print(f'yo dumbahh chose{user}, the computer chose{program'})
+print('yo dumbahh chose{user}, the computer chose{program}')
 
 if user == program:
-print('tied')
+ print('tied')
 
 elif user == 'rock':
-if program == 'scissors':
-print('you win u want a damn medal or smth?')
+ if program == 'scissors':
+  print('you win u want a damn medal or smth?')
 
 elif user == 'rock':
-if program == 'paper':
-print('you lost faggotino')
+ if program == 'paper':
+  print('you lost faggotino')
 
 elif user == 'paper':
-if program == 'rock':
-print('u win u want a damn medal or smth?')
+ if program == 'rock':
+  print('u win u want a damn medal or smth?')
 
 elif user == 'paper':
-if program == 'scissors':
-print('u fucking lost faggot u bad')
+ if program == 'scissors':
+  print('u fucking lost faggot u bad')
 
 elif user == 'scissors':
-if program == 'paper':
-print('HOLY SHIT U WON U SOME KIND OF MIND READER PSYCHIC OR SOME SHIT OR A WITCH?')
+ if program == 'paper':
+  print('HOLY SHIT U WON U SOME KIND OF MIND READER PSYCHIC OR SOME SHIT OR A WITCH?')
 
 elif user == 'scissors':
-if program == 'rock':
-print('U LOST GET BETTER')
+ if program == 'rock':
+  print('U LOST GET BETTER')
 
 
