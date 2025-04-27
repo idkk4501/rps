84de50c6499f98ea780af1f7b88e5cf80ad38f2d
idkk4501 <justinirvin951@gmail.com>
2025-04-26 20:39:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import random
+
+print('starting RPS...')
+
+user = input('rock paper or scissors?:')  
+pcch = ["rock", "paper", "scissors]
+program = random.choice(pcch)
+
+print(f'yo dumbahh chose{user}, the computer chose{program'})
+
+if user == program:
+print('tied')
+
+elif user == 'rock':
+if program == 'scissors':
+print('you win u want a damn medal or smth?')
+
+elif user == 'rock':
+if program == 'paper':
+print('you lost faggotino')
+
+elif user == 'paper':
+if program == 'rock':
+print('u win u want a damn medal or smth?')
+
 
